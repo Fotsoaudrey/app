c6342a177ed88ad7cb6d12db32148b5be600dcea
Fotsoaudrey <fotsoaudrey15@gmail.com>
2026-08-06 17:24:31 +0200

    mise a jour

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgres://apitacheuser:motDePasse@localhost:5432/apitache"
+DATABASE_URL = "postgresql ://apitacheuser:motDePasse@localhost:5432/apitache"
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(
     autocommit = False,
     autoflush = False,
     bind = engine
-)
+) 
